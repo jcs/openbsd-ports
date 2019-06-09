26624f5f26c2c93c67082858f7a1ba43bf4ca154
joshua stein <jcs@jcs.org>
2019-06-09 11:08:26 -0500

    add llvm6 port, which is just ports/devel/llvm from -D2019-01-27

---
+++ lang/clang6/clang6.port.mk
@@ -0,0 +1,62 @@
+# $OpenBSD: clang.port.mk,v 1.32 2019/01/28 06:27:28 jca Exp $
+
+MODCLANG_VERSION=	6.0.1
+
+MODCLANG_ARCHS ?= ${LLVM_ARCHS}
+MODCLANG_LANGS ?=
+
+.if !${MODCLANG_LANGS:L:Mc}
+# Always include support for this
+MODCLANG_LANGS += c
+.endif
+
+_MODCLANG_OKAY = c c++
+.for _l in ${MODCLANG_LANGS:L}
+.  if !${_MODCLANG_OKAY:M${_l}}
+ERRORS += "Fatal: unknown language ${_l}"
+.  endif
+.endfor
+
+_MODCLANG_ARCH_USES = No
+
+.for _i in ${MODCLANG_ARCHS}
+.  if !empty(MACHINE_ARCH:M${_i})
+_MODCLANG_ARCH_USES = Yes
+.  endif
+.endfor
+
+_MODCLANG_ARCH_CLANG = No
+
+.for _i in ${CLANG_ARCHS}
+.  if !empty(MACHINE_ARCH:M${_i})
+_MODCLANG_ARCH_CLANG = Yes
+.  endif
+.endfor
+
+.if ${_MODCLANG_ARCH_USES:L} == "yes"
+
+BUILD_DEPENDS += devel/llvm6>=${MODCLANG_VERSION}
+COMPILER_LINKS = gcc ${LOCALBASE}/bin/clang cc ${LOCALBASE}/bin/clang \
+	clang ${LOCALBASE}/bin/clang
+
+.  if ${MODCLANG_LANGS:L:Mc++}
+COMPILER_LINKS += g++ ${LOCALBASE}/bin/clang++ c++ ${LOCALBASE}/bin/clang++ \
+	clang++ ${LOCALBASE}/bin/clang++
+
+.    if ${_MODCLANG_ARCH_CLANG:L} == "no"
+# uses libestdc++
+MODULES += gcc4
+MODCLANG_CPPLIBDEP = ${MODGCC4_CPPLIBDEP}
+LIB_DEPENDS += ${MODCLANG_CPPLIBDEP}
+MODCLANG_CPPWANTLIB = ${MODGCC4_CPPWANTLIB}
+WANTLIB += ${MODCLANG_CPPWANTLIB}
+.    else
+# uses libc++
+MODCLANG_CPPLIBDEP =
+MODCLANG_CPPWANTLIB = c++ c++abi pthread
+WANTLIB += ${MODCLANG_CPPWANTLIB}
+.    endif
+.  endif
+.endif
+
+SUBST_VARS+=	MODCLANG_VERSION
